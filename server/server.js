54bfedf7d5c39222977a2eb43c27257acf910e76
shashank36 <s.shashank36@gmail.com>
2018-04-03 15:13:35 +0530

    Send message to only chat rooms they are in with User name

--- server/server.js
+++ server/server.js
@@ -1,83 +1,67 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const http = require('http');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
 const {Users} = require('./utils/users');
 
-const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 var users = new Users();
-
-var io = socketIO.listen(server);
-io.on('connection',(socket) => {
-	console.log("New user connected !");
-
-	
-
-
-	socket.on('join',(params,callback) =>{
-		console.log(params);
-		if(!isRealString(params.name) || !isRealString(params.room)){
-			return callback('Name and Room Name required.')
-		}
-
-		socket.join(params.room);
-		users.removeUser(socket.id);
-		users.addUser(socket.id,params.name,params.room);
-
-		io.to(params.room).emit('updateUserList',users.getUserList(params.room));
-		// New user joins in 
-		socket.emit('newMessage', generateMessage('Admin','Welcome new user'));
-
-		// Broadcast that a new user is here
-		socket.broadcast.emit('newMessage',generateMessage('Admin', `${params.name} has joined the room`));
-
-
-
-
-		callback();
-	});
-
-
-
-	//logging a user message in terminal
-	socket.on('createMessage', (message,callback) =>{
-		console.log('createMessage', message);
-		callback("server got the message");
-		io.emit('newMessage',generateMessage(message.from,message.text));
-
-		// socket.broadcast.emit('newMessage',{
-		// 	from: message.from,
-		// 	text: message.text
-		// })
-		//  actual message from one user to another
-	});
-
-	socket.on('createLocationMessage',(coords) => {
-		io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
-	});
-
-
-	socket.on('disconnect', () => {
-		var user = users.removeUser(socket.id);
-
-		if (user) {
-			io.to(user.room).emit('updateUserList',users.getUserList(user.room));
-			io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left.`));
-		}
-	});
-});
-
-
 
 app.use(express.static(publicPath));
 
-server.listen(port,() => {
-	console.log("Server is running!!");
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+
+  socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
+    callback();
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+    }
+  });
+
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
